a7db8d8bd9ded54be7933838f5d2badbda87d283
Glitch (tricolor-nebulous-jellyfish) <none>
2021-01-14 23:25:28 +0000

    🍤👟 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -5,5 +5,25 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+let userChoice = "";
+let computerChoice = "";
+let winner = "";
+let randomNumber = 0;
 
+$("button").click(function() {
+  randomNumber = Math.random();
+  userChoice = $("input").val();
+  $("#userChoice").text(userChoice);
+  
+  if (randomNumber > 0.8) {
+    computerChoice = "paper";
+  } else if (randomNumber > 0.2) {
+    computerChoice = "rock";
+  } else {
+    computerChoice = "scissors";
+  }
+  
+  $("#computerChoice").text(computerChoice)
+  
+});
 
